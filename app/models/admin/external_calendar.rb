0ef7a5140f853dfe3ba23b5d1879fcabcd008341
Kevin Turnbull <bitbucket@kevinturnbull.ca>
2016-01-17 11:38:14 -0500

    Fixing bug #33 which downcased the urls breaking uids

--- app/models/admin/external_calendar.rb
+++ app/models/admin/external_calendar.rb
@@ -5,10 +5,11 @@ class Admin::ExternalCalendar < ActiveRecord::Base
 
   validates :url, presence: true, uniqueness: true
 
-  before_save -> { self.url = url.downcase }
+  # KT@HOME - 2016/01/17: Issue #33 This is broken for links that use generated UIDs such as (i dunno... FACEBOOK LINKS FOR EXAMPLE?!)
+  # before_save -> { self.url = url.downcase }
 
   def to_s
-    "#{ title.titleize }"
+    "#{ title }"
   end
 
   alias_method :name, :to_s

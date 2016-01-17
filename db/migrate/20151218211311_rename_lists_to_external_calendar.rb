f86e892c1507926198c995e98bba80fdd1ad3fd2
Kevin Turnbull <bitbucket@kevinturnbull.ca>
2016-01-17 14:07:08 -0500

    Adding some more finishing touches to get #33 fixed

--- db/migrate/20151218211311_rename_lists_to_external_calendar.rb
+++ db/migrate/20151218211311_rename_lists_to_external_calendar.rb
@@ -1,5 +1,6 @@
 class RenameListsToExternalCalendar < ActiveRecord::Migration
   def change
     rename_table :admin_lists, :admin_external_calendars
+    rename_column :events, :list_id, :external_calendar_id
   end
 end
